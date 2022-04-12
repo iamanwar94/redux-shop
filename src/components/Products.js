936865074214a17b8e1cc9abed9a8481c1259b9e
iamanwar94 <90406643+iamanwar94@users.noreply.github.com>
2022-04-12 10:43:49 +0500

    second commit

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { add } from '../store/cartSlice';
 import { fetchProducts } from '../store/productSlice';
@@ -18,7 +18,7 @@ const Products = () => {
         //     setProducts(data);
         // };
         // fetchProducts();
-    }, []);
+    });
 
     const handleAdd = (product) => {
         dispatch(add(product));
